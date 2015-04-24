4835646f93f1dfcc113ce3cc7be6eac36d29a4c0
localandwellness <lpitts1384@gmail.com>
2015-04-24 18:14:50 -0500

    new code
    the rest of the code

--- app/src/main/java/com/example/gerad/localhealth/Ailments.java
+++ app/src/main/java/com/example/gerad/localhealth/Ailments.java
@@ -2,28 +2,65 @@ package com.example.gerad.localhealth;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+
 /**
  * Created by gerad on 4/21/2015.
  */
+
 public class Ailments extends Activity {
+
+    CheckBox checkBox;
+    Button button1;
+    Button button2;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ailments);
-        OnClickListener Listener = new OnClickListener() {
-test
-
-            @Override
-            public void onClick(View view) {
 
 
-            }
-        };
-    }}
+            View.OnClickListener listener1 = new View.OnClickListener() {
+                public void onClick(View view) {
+
+                    Intent intent = new Intent(view.getContext(), Allergies.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+            };
+
+            View.OnClickListener listener2 = new OnClickListener() {
+                public void onClick(View view) {
+
+                    Intent intent = new Intent(view.getContext(), Allergies.class);
+                    startActivity(intent);
+                    finish();
+                }
+            };
+
+            View.OnClickListener listener3 = new OnClickListener() {
+                public void onClick(View view) {
+
+                    Intent intent = new Intent(view.getContext(), Allergies.class);
+                    startActivity(intent);
+                    finish();
+                }
+            };
+
+            button1.setOnClickListener(listener1);
+            button2.setOnClickListener(listener2);
+            button3.setOnClickListener(listener3);
+
+
+        }
+    }
 
 
 
